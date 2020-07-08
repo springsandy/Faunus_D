7587317fca429fde6ee46ea9f9f83545b7c314a7
s2018w26 <s2018w26@e-mirim.hs.kr>
2020-07-08 12:42:43 +0900

    fourth

--- dictionary/views.py
+++ dictionary/views.py
@@ -15,12 +15,46 @@ class DictionaryList(ListView):
             dictionary_list = self.model.objects.all()
         else:
             dictionary_list = self.model.objects.filter(animal_name__contains=search_animal_name)
+        return dictionary_list
 
+class DictionaryFishList(ListView):
+    model = Dictionary
+    paginate_by = 10
+    def get_queryset(self):
+        dictionary_list = self.model.objects.filter(kind='어류')
+        return dictionary_list
+
+class DictionaryAmphibiaList(ListView):
+    model = Dictionary
+    paginate_by = 10
+    def get_queryset(self):
+        dictionary_list = self.model.objects.filter(kind='양서류')
+        return dictionary_list
+
+class DictionaryReptileList(ListView):
+    model = Dictionary
+    paginate_by = 10
+    def get_queryset(self):
+        dictionary_list = self.model.objects.filter(kind='파충류')
+        return dictionary_list
+
+class DictionaryMammalList(ListView):
+    model = Dictionary
+    paginate_by = 10
+    def get_queryset(self):
+        dictionary_list = self.model.objects.filter(kind='포유류')
+        return dictionary_list
+
+class DictionaryBirdList(ListView):
+    model = Dictionary
+    paginate_by = 10
+    def get_queryset(self):
+        dictionary_list = self.model.objects.filter(kind='조류')
         return dictionary_list
 
 class DictionaryCreateView(CreateView):
     model = Dictionary
-    fields = ['animal_name', 'level', 'age', 'size', 'eat', 'live', 'description']
+    fields = '__all__'
     template_name_suffix = '_create'
     success_url = reverse_lazy('dictionary:list')
 
@@ -29,7 +63,7 @@ class DictionaryDetailView(DetailView):
 
 class DictionaryUpdateView(UpdateView):
     model = Dictionary
-    fields = ['animal_name', 'level', 'age', 'size', 'eat', 'live', 'description']
+    fields = '__all__'
     template_name_suffix = '_update'
     success_url = reverse_lazy('dictionary:list')
 
